fd5f63a3ddac964e0d142888d45cb624efa68349
Wenfei Lin <wl688728@gmail.com>
2025-07-18 11:17:59 -0400

    Implement createGrid function

---
+++ script.js
@@ -0,0 +1,20 @@
+const container = document.querySelector(".container");
+
+function createGrid(gridLength = 16) {
+  const gridSize = gridLength * gridLength;
+
+  // Container is a square.
+  let containerLength = container.offsetWidth;
+  let squareLength = Math.floor((containerLength - 32)/ gridLength);
+
+  for (let i = 0; i < gridSize; i++) {
+    let squareDiv = document.createElement("div");
+    squareDiv.classList.toggle("square");
+  
+    squareDiv.style.height = `${squareLength}px`;
+    squareDiv.style.width = `${squareLength}px`;
+    container.appendChild(squareDiv);
+  }
+}
+
+createGrid();
